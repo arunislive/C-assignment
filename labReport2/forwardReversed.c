e334777c9ebda84dcba4b707b563a277790910fb
Arun Khanal <zarunkhanal1@gmail.com>
2024-12-07 13:54:31 +0545

    created a repo for my lab assignment 1 and 2

---
+++ labReport2/forwardReversed.c
@@ -0,0 +1,11 @@
+// Write a program to prompt the user to input 3 integer values and print these values in forward and reversed order.
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d%d%d", &a, &b, &c);
+    printf("\n");
+    printf("Your input is: %d\t%d\t%d\n", a, b, c);
+    printf("Your input in reversed order is: %d\t%d\t%d\n", c, b, a);
+}
