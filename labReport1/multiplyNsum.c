6c856d9b1c761267886b95318714a0325d4c6053
Arun Khanal <zarunkhanal1@gmail.com>
2024-12-07 18:10:11 +0545

    third qn with return 0

---
+++ labReport1/multiplyNsum.c
@@ -0,0 +1,8 @@
+// Write a program to multiply two numbers (10&8) and display its sum.
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 8;
+    printf("a = %d, b = %d, a*b = %d. a+b = %d", a, b, a * b, a + b);
+    return 0;
+}
--- labReport1/sum.c
+++ labReport1/sum.c
@@ -7,4 +7,5 @@ int main()
     printf("Enter 2 numbers:");
     scanf("%d%d", &a, &b);
     printf("sum of %d and %d is %d:", a, b, a + b);
+    return 0;
 }
