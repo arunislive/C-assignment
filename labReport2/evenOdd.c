c9e09c7f144ceb8eab48181d47e5603c790fd812
Arun Khanal <zarunkhanal1@gmail.com>
2024-12-08 21:02:32 +0545

    used ternary instead of generic if else

--- labReport2/evenOdd.c
+++ labReport2/evenOdd.c
@@ -3,51 +3,34 @@
 //?(b) using bitwise operator
 //?(c) without using bitwise and modulus operator
 //?(d) using conditional operator.
-
 #include <stdio.h>
 
 int main()
 {
     int num;
 
-    // Input a number
     printf("Enter a number: ");
     scanf("%d", &num);
 
     // (a) Using Modulus Operator
-    if (num % 2 == 0)
-    {
-        printf("(a) %d is even (using modulus).\n", num);
-    }
-    else
-    {
-        printf("(a) %d is odd (using modulus).\n", num);
-    }
+    (num % 2 == 0)
+        ? printf("(a) Using modulus: %d is even.\n", num)
+        : printf("(a) Using modulus: %d is odd.\n", num);
 
     // (b) Using Bitwise Operator
-    if ((num & 1) == 0)
-    {
-        printf("(b) %d is even (using bitwise).\n", num);
-    }
-    else
-    {
-        printf("(b) %d is odd (using bitwise).\n", num);
-    }
+    (num & 1)
+        ? printf("(b) Using bitwise: %d is odd.\n", num)
+        : printf("(b) Using bitwise: %d is even.\n", num);
 
-    // (c) Without Using Bitwise or Modulus Operator
-    if ((num / 2) * 2 == num)
-    {
-        printf("(c) %d is even (without modulus or bitwise).\n", num);
-    }
-    else
-    {
-        printf("(c) %d is odd (without modulus or bitwise).\n", num);
-    }
+    // (c) Without Using Modulus or Bitwise Operator
+    ((num / 2) * 2 == num)
+        ? printf("(c) Without modulus or bitwise: %d is even.\n", num)
+        : printf("(c) Without modulus or bitwise: %d is odd.\n", num);
 
     // (d) Using Conditional Operator
     (num % 2 == 0)
-        ? printf("(d) %d is even (using conditional).\n", num)
-        : printf("(d) %d is odd (using conditional).\n", num);
+        ? printf("(d) Using conditional operator: %d is even.\n", num)
+        : printf("(d) Using conditional operator: %d is odd.\n", num);
 
     return 0;
 }
